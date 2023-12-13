086b5ff7a5051e252495dca40e1ee992cf52d599
Yazan Abu Khalil <yazan.a.abukhalil@gmail.com>
2023-12-13 13:07:56 +0300

    remove logout method from DashBoardPage file

--- cypress/pages/DashBoardPage.ts
+++ cypress/pages/DashBoardPage.ts
@@ -5,10 +5,5 @@ class DashBoardPage extends BasePage {
     PIMTab: string = "[href='/web/index.php/pim/viewPimModule']";
     dropdownMenue: string = ".oxd-userdropdown-tab";
     logoutButton: string = ".oxd-dropdown-menu > :last-child";
-
-    logout() {
-        this.clickElement(this.dropdownMenue);
-        this.clickElement(this.logoutButton);
-    }
 }
 export = new DashBoardPage();
