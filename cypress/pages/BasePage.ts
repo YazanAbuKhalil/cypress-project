f4333435703345e15acedea1b7f66cf950441527
Yazan Abu Khalil <yazan.a.abukhalil@gmail.com>
2023-12-13 12:42:49 +0300

    make BasePage file with general methods

---
+++ cypress/pages/BasePage.ts
@@ -0,0 +1,39 @@
+class BasePage {
+    minNumber: number = 0;
+	maxNumber: number = 1000000000;
+
+    navigateTo(url: string) {
+        cy.visit(url);
+    }
+
+    currentPath() {
+        return cy.url();
+    }
+    
+    getElement(locator: string, timeoutValue?: any) {
+		  return cy.get(locator, { timeout: timeoutValue });
+	}
+
+    clickElement(locator: string) {
+        this.getElement(locator).click();
+    }
+
+    typeInput(locater: string, value: string) {
+        this.getElement(locater).type(value);
+    }
+
+    generateRandomNumber(min = this.minNumber, max = this.maxNumber): number {
+		return Math.floor(Math.random() * max) + min;
+	}
+
+    getRandomUserName(): string {
+        return `test_username_${this.generateRandomNumber()}`;
+    }
+
+
+    getRandomPassword(): string {
+        return `test_Automation_${this.generateRandomNumber()}`;
+    }
+
+}
+export = BasePage;
