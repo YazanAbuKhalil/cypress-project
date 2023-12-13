b766f63559bc8e8a69bd1ffa26f45741b32b5fba
Yazan Abu Khalil <yazan.a.abukhalil@gmail.com>
2023-12-13 19:54:16 +0300

    remove searchEmployee function

--- cypress/pages/EmployeeListPage.ts
+++ cypress/pages/EmployeeListPage.ts
@@ -1,14 +1,8 @@
 import BasePage from "./BasePage";
 
 class EmployeeListPage extends BasePage {
-    employeeIdInput = ".oxd-form-row .oxd-grid-4 > :nth-child(2) input";
-    searchButton = "[type='submit']";
-    employeeIdField = ".oxd-table-body .oxd-table-row > :nth-child(2) div";
-
-    searchEmployee(id="1111") {
-        this.getElement(this.employeeIdInput).type(id);
-        this.clickElement(this.searchButton);
-    }
+  employeeIdInput = ".oxd-form-row .oxd-grid-4 > :nth-child(2) input";
+  searchButton = "[type='submit']";
+  employeeIdField = ".oxd-table-body .oxd-table-row > :nth-child(2) div";
 }
 export = new EmployeeListPage();
-
